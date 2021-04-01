2528622cc1ecfa9c5d72b1384771fe8476299649
AlexKwangwoo <bnc3049@gmail.com>
2021-03-31 20:44:19 -0600

    backsetNew1

--- src/payments/dtos/get-payments.dto.ts
+++ src/payments/dtos/get-payments.dto.ts
@@ -2,16 +2,8 @@
 // import { CoreOutput } from 'src/common/dtos/output.dto';
 // import { Payment } from '../entities/payment.entity';
 
-<<<<<<< HEAD
-@ObjectType()
-export class GetPaymentsOutput extends CoreOutput {
-  @Field((type) => [Payment], { nullable: true })
-  payments?: Payment[];
-}
-=======
 // @ObjectType()
 // export class GetPaymentsOutput extends CoreOutput {
 //   @Field(type => [Payment], { nullable: true })
 //   payments?: Payment[];
 // }
->>>>>>> 39f8045ec07073402bd21305d073d1242219ec3c
